13f790fca13e768ec307a6f011e1830693c57ba6
kyy <kytomyleo@gmail.com>
2021-07-27 14:19:46 +0700

    first commit

---
+++ src/test/java/heroku/demoheroku/DemoherokuApplicationTests.java
@@ -0,0 +1,13 @@
+package heroku.demoheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoherokuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
